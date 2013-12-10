26841e75f2cbdeba86230edb1a42975c3b5acfc2
Sam Ellis <github@elliz.com>
2013-12-10 01:17:34 +0000

    Tidied up redirects

---
+++ _redirects/portfolio/_generate.bat
@@ -0,0 +1,238 @@
+echo off
+
+set filename=022050.htm
+call _generateone.bat
+
+set filename=543203f.htm
+call _generateone.bat
+
+set filename=543204.htm
+call _generateone.bat
+
+set filename=567602b.htm
+call _generateone.bat
+
+set filename=567603b.htm
+call _generateone.bat
+
+set filename=567604b.htm
+call _generateone.bat
+
+set filename=586302Zz.htm
+call _generateone.bat
+
+set filename=747nobl2.htm
+call _generateone.bat
+
+set filename=ag225007.htm
+call _generateone.bat
+
+set filename=ag225b23.htm
+call _generateone.bat
+
+set filename=art.htm
+call _generateone.bat
+
+set filename=av333006.htm
+call _generateone.bat
+
+set filename=av37749.htm
+call _generateone.bat
+
+set filename=ba362803.htm
+call _generateone.bat
+
+set filename=bc022034.htm
+call _generateone.bat
+
+set filename=bc307052.htm
+call _generateone.bat
+
+set filename=bs381337.htm
+call _generateone.bat
+
+set filename=bv06424.htm
+call _generateone.bat
+
+set filename=bv307126.htm
+call _generateone.bat
+
+set filename=c307118b.htm
+call _generateone.bat
+
+set filename=Chocolate.htm
+call _generateone.bat
+
+set filename=ck02203.htm
+call _generateone.bat
+
+set filename=cn377444.htm
+call _generateone.bat
+
+set filename=commercial.htm
+call _generateone.bat
+
+set filename=communications.htm
+call _generateone.bat
+
+set filename=Comp4bw.htm
+call _generateone.bat
+
+set filename=cr307029.htm
+call _generateone.bat
+
+set filename=cr37742.htm
+call _generateone.bat
+
+set filename=cs280401.htm
+call _generateone.bat
+
+set filename=ec377413.htm
+call _generateone.bat
+
+set filename=ec436226.htm
+call _generateone.bat
+
+set filename=em377416.htm
+call _generateone.bat
+
+set filename=environment.htm
+call _generateone.bat
+
+set filename=ft022024.htm
+call _generateone.bat
+
+set filename=fvs30714.htm
+call _generateone.bat
+
+set filename=gc307121.htm
+call _generateone.bat
+
+set filename=gr280b01.htm
+call _generateone.bat
+
+set filename=Hands2.htm
+call _generateone.bat
+
+set filename=Helidawn.htm
+call _generateone.bat
+
+set filename=hightech.htm
+call _generateone.bat
+
+set filename=hv377604.htm
+call _generateone.bat
+
+set filename=HVS2x.htm
+call _generateone.bat
+
+set filename=jl436221.htm
+call _generateone.bat
+
+set filename=jm377410.htm
+call _generateone.bat
+
+set filename=mm022042.htm
+call _generateone.bat
+
+set filename=na30722b.htm
+call _generateone.bat
+
+set filename=nc064231.htm
+call _generateone.bat
+
+set filename=nc280403.htm
+call _generateone.bat
+
+set filename=nc307058.htm
+call _generateone.bat
+
+set filename=nc642.htm
+call _generateone.bat
+
+set filename=nn02204.htm
+call _generateone.bat
+
+set filename=nn307030.htm
+call _generateone.bat
+
+set filename=norlaser.htm
+call _generateone.bat
+
+set filename=oilrig.htm
+call _generateone.bat
+
+set filename=os07053a.htm
+call _generateone.bat
+
+set filename=os07053d.htm
+call _generateone.bat
+
+set filename=ps427712.htm
+call _generateone.bat
+
+set filename=ra37721b.htm
+call _generateone.bat
+
+set filename=rb073908.htm
+call _generateone.bat
+
+set filename=rb778b24.htm
+call _generateone.bat
+
+set filename=sa4682b3.htm
+call _generateone.bat
+
+set filename=sc022016.htm
+call _generateone.bat
+
+set filename=sh226002.htm
+call _generateone.bat
+
+set filename=sh436219.htm
+call _generateone.bat
+
+set filename=Tower1.htm
+call _generateone.bat
+
+set filename=tw230303.htm
+call _generateone.bat
+
+set filename=tw280405.htm
+call _generateone.bat
+
+set filename=tw307130.htm
+call _generateone.bat
+
+set filename=tw381201.htm
+call _generateone.bat
+
+set filename=tw381204.htm
+call _generateone.bat
+
+set filename=tw381210.htm
+call _generateone.bat
+
+set filename=tw381213.htm
+call _generateone.bat
+
+set filename=tw381214.htm
+call _generateone.bat
+
+set filename=tw381224.htm
+call _generateone.bat
+
+set filename=tw381234.htm
+call _generateone.bat
+
+set filename=wr307026.htm
+call _generateone.bat
+
+set filename=wr427704.htm
+call _generateone.bat
+
+set filename=wr427709.htm
+call _generateone.bat
+
+echo finished
+pause
